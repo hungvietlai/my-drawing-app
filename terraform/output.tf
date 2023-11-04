e5d9f39f267b8a6a50b5db6a6403bdefaf98f752
hungvietlai <hungvietlai@hotmail.com>
2023-11-04 17:19:06 +0000

    Updated output.tf

---
+++ terraform/output.tf
@@ -0,0 +1,30 @@
+
+output "cluster_endpoint" {
+  value       = module.eks.cluster_endpoint
+  description = "The endpoint for your EKS Kubernetes API."
+}
+
+output "kubeconfig_certificate_authority_data" {
+  value       = module.eks.kubeconfig_certificate_authority_data
+  description = "The base64 encoded certificate data required to communicate with your cluster."
+}
+
+output "subnet_ids" {
+  value       = module.eks.subnet_ids
+  description = "The IDs of the subnets used by the EKS cluster"
+}
+
+output "security_group_id" {
+  value       = module.eks.security_group_id
+  description = "The ID of the security group used by the EKS cluster"
+}
+
+output "eks_worker_instance_profile_arn" {
+  value       = module.eks.eks_worker_instance_profile_arn
+  description = "The ARN of the IAM instance profile used by the EKS worker nodes"
+}
+
+output "node_group_arn" {
+  value       = module.eks.node_group_arn
+  description = "The ARN of the EKS node group"
+}
