6b6177d20d2cd6269c90f2df6a3f8bc0bf373528
hungvietlai <hungvietlai@hotmail.com>
2023-11-06 23:15:42 +0000

    added subnets

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,10 +10,16 @@ variable "vpc_id" {
   default = "vpc-0eaac7dbfa87419c1"
 }
 
-variable "subnet_cidr" {
+variable "subnet1_cidr" {
   type        = list(string)
   description = "A list of CIDR blocks for the subnets in the EKS cluster."
-  default = [ "192.168.1.0/24", "192.168.2.0/24" ]
+  default = [ "192.168.0.0/24", "192.168.1.0/24" ]
+}
+
+variable "subnet2_cidr" {
+  type        = list(string)
+  description = "A list of CIDR blocks for the subnets in the EKS cluster."
+  default = [ "192.168.2.0/24", "192.168.3.0/24" ]
 }
 
 variable "subnet_AZ" {
